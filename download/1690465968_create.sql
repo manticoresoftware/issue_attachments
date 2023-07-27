b7553d43affe16b508a5dbf469e30110630cba58
Manticore Team <42860502+githubmanticore@users.noreply.github.com>
2023-07-27 20:52:49 +0700

    fileUpload

---
+++ download/1690465968_create.sql
@@ -0,0 +1,17 @@
+create table test1 ( title text, tag integer );
+insert into test1 values (1, 'word form', 11);
+flush ramchunk test1;
+insert into test1 values (2, 'word form', 11);
+flush ramchunk test1;
+insert into test1 values (3, 'word form', 11);
+flush ramchunk test1;
+insert into test1 values (4, 'word form', 11);
+flush ramchunk test1;
+insert into test1 values (5, 'word form', 11);
+flush ramchunk test1;
+insert into test1 values (6, 'word form', 11);
+flush ramchunk test1;
+insert into test1 values (7, 'word form', 11);
+flush ramchunk test1;
+insert into test1 values (8, 'word form', 11);
+flush ramchunk test1;
